8c4562123b9622cced097f1ff2837ed3cdc8554c
Ankit Singh <asingh22603@gmail.com>
2026-05-27 14:23:03 +0530

    Create Login Form and redirect to Home Page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Login
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // handle input
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // handle submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        formData,
+      );
+
+      //save token
+      localStorage.setItem("token", response.data.token);
+
+      console.log(response.data);
+
+      //redirect
+      navigate("/");
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Enter email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="password"
+          placeholder="Enter password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
